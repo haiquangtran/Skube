fdbcb2ec96aaaf77752ce63c47347e20b0305e24
haiquangtran <hai.quang.tran@gmail.com>
2016-08-28 00:32:27 +1200

    rotation of cubes and added material

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -8,21 +8,18 @@ namespace Assets.Scripts
     public static class Constants
     {
         public static class World
-        {
-            // NOTE:
-            // Camera perspective is 0,0,0
-            
+        { 
             // Enemies
-            public const int NUM_OF_ENEMIES = 100;
+            public const int NUM_OF_ENEMIES = 20;
             public const int ENEMY_WIDTH = 1; 
             public const float ENEMY_SPEED = -0.1f;
             
             // Bounds of the world
-            public const int MIN_X = -100;
-            public const int MAX_X = 100;
+            public const int MIN_X = -50;
+            public const int MAX_X = 50;
             public const int MIN_Y = 0;
             public const int MAX_Y = 100;
-            public const int MIN_Z = 0;
+            public const int MIN_Z = -50;
             public const int MAX_Z = 50;
         }
 
