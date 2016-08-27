704b418106f8f5dc7dd53e9189d4b5f45734ed0b
mark-laptop\mark <marktaglieri@hotmail.com>
2016-08-27 19:55:27 +1200

    Start menu!

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
