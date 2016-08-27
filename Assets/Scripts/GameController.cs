ca370bc66a2adf7623f4b87f1e4bcf3b29f1308e
mark <marktaglieri@hotmail.com>
2016-08-27 16:44:57 +1200

    Skube enemy cubes collide with player cube.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,20 +5,32 @@ public class GameController : MonoBehaviour {
 
     // Use this for initialization
     public GameObject prefab;
-    private ArrayList cubes = new ArrayList();
+    private ArrayList enemyCubes = new ArrayList();
+    private GameObject playerCube;
 
     void Start()
     {
         for (int i = 0; i < 10; i++)
-            cubes.Add(Instantiate(prefab, new Vector3(i * 2.0f - 10.0f, 0, 0), Quaternion.identity));
+        {
+            enemyCubes.Add(Instantiate(prefab, new Vector3(i * 2.0f - 10.0f, 0, 0), Quaternion.identity));
+        }
+
+        //player cube
+        playerCube = (GameObject)Instantiate(prefab, new Vector3(0, 0, -5), Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update () {
-	    foreach(GameObject c in cubes)
+	    foreach(GameObject c in enemyCubes)
         {
-            Vector3 temp = new Vector3(0, 0, -0.3f);
+            Vector3 temp = new Vector3(0, 0, -0.05f);
             c.transform.position += temp;
         }
 	}
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log("collided");
+    }
+
 }
