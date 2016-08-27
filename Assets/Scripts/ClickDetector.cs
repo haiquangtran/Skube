ea96073564b80ab59d2b0f6f80e533340d12fcf5
BillGates.avi <avimish62@gmail.com>
2016-08-27 23:06:02 +1200

    we like to shoot

---
+++ Assets/Scripts/ClickDetector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickDetector : MonoBehaviour {
+    public int button = 0;
+    public float clickSize = 50; // this might be too small
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+    
+    void ClickHappened()
+    {
+        Debug.Log("CLICK!");
+    }
+
+    Vector3 pos;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(button))
+            pos = Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(button))
+        {
+            var delta = Input.mousePosition - pos;
+            if (delta.sqrMagnitude < clickSize * clickSize)
+                ClickHappened();
+        }
+    }
+}
