ca95a4e1e9448b19e7a998f8634fbc88742983af
haiquangtran <hai.quang.tran@gmail.com>
2016-08-27 20:16:43 +1200

    always show enemies in scene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,12 +11,7 @@ public class GameController : MonoBehaviour {
 
     public void Start()
     {
-        for (int i = 0; i < Constants.World.NUM_OF_ENEMIES; i++)
-        {
-            var enemyGap = Constants.World.ENEMY_WIDTH;
-            var offsetToMiddle = (Constants.World.NUM_OF_ENEMIES / 2) * (Constants.World.ENEMY_WIDTH + enemyGap);
-            enemyCubes.Add(Instantiate(enemyPrefab, new Vector3(i * (Constants.World.ENEMY_WIDTH + enemyGap) - offsetToMiddle, 0, Constants.World.MAX_Z), Quaternion.identity));
-        }
+        GenerateEnemyCubes();
     }
 
     // Update is called once per frame
@@ -29,7 +24,10 @@ public class GameController : MonoBehaviour {
             Vector3 newPosition = new Vector3(0, 0, Constants.World.ENEMY_SPEED);
             enemyCube.transform.position += newPosition;
 
-            DeleteOutOfBoundsEnemy(enemyCube);
+            if (IsOutOfWorldBounds(enemyCube))
+            {
+                deletedEnemyCubes.Add(enemyCube);
+            }
         }
 
         foreach (GameObject enemy in deletedEnemyCubes)
@@ -37,16 +35,28 @@ public class GameController : MonoBehaviour {
             enemyCubes.Remove(enemy);
             Destroy(enemy);
         }
+
+        // Generate new enemies
+        if (enemyCubes.Count == 0)
+        {
+            GenerateEnemyCubes();
+        }
 	}
 
-    private void DeleteOutOfBoundsEnemy(GameObject enemyCube)
+    private bool IsOutOfWorldBounds(GameObject cube)
     {
-        var enemyPosition = enemyCube.transform.position;
-        if (enemyPosition.z < Constants.World.MIN_Z || enemyPosition.z > Constants.World.MAX_Z ||
-            enemyPosition.x < Constants.World.MIN_X || enemyPosition.x > Constants.World.MAX_X || 
-            enemyPosition.y < Constants.World.MIN_Y || enemyPosition.y > Constants.World.MAX_Y)
+        var enemyPosition = cube.transform.position;
+        return enemyPosition.z < Constants.World.MIN_Z || enemyPosition.z > Constants.World.MAX_Z || enemyPosition.x < Constants.World.MIN_X || 
+            enemyPosition.x > Constants.World.MAX_X || enemyPosition.y < Constants.World.MIN_Y || enemyPosition.y > Constants.World.MAX_Y;
+    }
+
+    private void GenerateEnemyCubes()
+    {
+        for (int i = 0; i < Constants.World.NUM_OF_ENEMIES; i++)
         {
-            deletedEnemyCubes.Add(enemyCube);
+            var enemyGap = Constants.World.ENEMY_WIDTH * 2;
+            var offsetToMiddle = (Constants.World.NUM_OF_ENEMIES / 2) * (Constants.World.ENEMY_WIDTH + enemyGap);
+            enemyCubes.Add(Instantiate(enemyPrefab, new Vector3(i * (Constants.World.ENEMY_WIDTH + enemyGap) - offsetToMiddle, 0, Constants.World.MAX_Z), Quaternion.identity));
         }
     }
 
